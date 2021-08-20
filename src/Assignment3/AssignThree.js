7063894e8c9592a965999e3e2edd8fc71766132b
sunita <sunita@metromaxsolutions.com>
2021-08-20 20:50:16 +0530

    changes done

--- src/Assignment3/AssignThree.js
+++ src/Assignment3/AssignThree.js
@@ -6,32 +6,18 @@ import {connect} from "react-redux";
 import {withRouter} from 'react-router';
 import ReactRegionSelect from 'react-region-select';
 import {Button} from "@material-ui/core";
-import {
-  JsonToCsv,
-  useJsonToCsv
-} from 'react-json-csv';
-import CsvDownload from 'react-json-to-csv'
 
-function CsvConverter (data){
-  const filename = 'Image-Data',
-  fields = {
-    "Name": "Name",
-    "TopLeft": "Top Left",
-    "BottomRight": "Bottom Right"
-  };
-  useJsonToCsv({data,fields,filename});
-  console.log(data,filename,fields)
+import CsvDownload from 'react-json-to-csv';
 
-}
 export default class AssignThree extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            data: [],
             regions: [],
-           
-           hideCancelButton: true,
-         
+            hideCancelButton: true,
+
         };
     }
 
@@ -41,54 +27,59 @@ export default class AssignThree extends Component {
             hideCancelButton: false
         })
     }
- handleChange = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  };
+    handleChange = (e) => {
+        e.preventDefault();
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value,
+        });
+    };
     hideCancelButton = () => {
         this.setState({
             hideCancelButton: true
         })
     }
 
-
     onChange = regions => {
-      
+
         this.setState({
             regions: regions,
-            
+
         });
     };
-    enterInput=(e,index)=>{
-      e.preventDefault();
-     const regions=this.state.regions;
+    enterInput = (e, index) => {
+        e.preventDefault();
+        const regions = this.state.regions;
 
-     for(let i of regions ){
-         if(i.data.index===index){
-           i.regionName=e.target.value;
-         }
+        for (let i of regions) {
+            if (i.data.index === index) {
+                i.regionName = e.target.value;
+            }
+        }
+        this.setState({regions});
+
+    }
+    createTextFile = () => {
+        let data = []
+        for (let i of this.state.regions) {
+            let a = {};
+            if (i.regionName) {
+                a.Name = i.regionName;
+                a.TopLeft = Math.round(i.x) + "," + Math.round(i.y);
+                a.BottomRight = Math.round((i.x + i.width)) + "," + Math.round((i.y + i.height));
+                data.push(a);
+            }
+
+        }
+        this.setState({
+            data: data, disableDownloadButton: false,
+        })
+     if(data.length==0){
+       alert("please select a region first and enter a name in the input")
      }
-    this.setState({regions});
-    
+     else  alert("Data generated!!! Click on Download data to generate a text file with data")
     }
-    createTextFile=()=>{
-    let data=[]
-    for(let i of this.state.regions){
-      let a={};
-      if(i.regionName){
-        a.Name=i.regionName;
-        a.TopLeft=Math.round(i.x)+","+Math.round(i.y);
-        a.BottomRight=Math.round(i.x+i.width)+","+Math.round(i.y+i.height);
-        data.push(a);
-      }
-      CsvConverter(data);
-    }
-    
-    }
-  
+
 
     actionDeleteRegion = regionIdx => {
         console.log("regionIdx", regionIdx);
@@ -104,39 +95,47 @@ export default class AssignThree extends Component {
             return (
                 <div>
                     <div style={{position: "absolute", right: 0, top: "-25px"}} hidden={this.state.hideCancelButton}>
-                        <Button  variant="contained" color="secondary"
-                            onClick={() => this.actionDeleteRegion(regionProps.data.index)}
+                        <Button variant="contained" color="secondary"
+                                onClick={() => this.actionDeleteRegion(regionProps.data.index)}
                         >
                             X
                         </Button>
                     </div>
-                    <input autoFocus onChange={(e)=>this.enterInput(e,regionProps.data.index)} placeholder={"Enter label"} className={"hiddeninput"}/>
+                    <input autoFocus onChange={(e) => this.enterInput(e, regionProps.data.index)}
+                           placeholder={"Enter label"} className={"hiddeninput"}/>
                 </div>
             );
         }
     };
 
-    actionAddRegion = () => {
-        alert("actionAddRegion");
-    };
+  
 
-   
 
     render() {
         const regionStyle = {
             background: "rgba(0, 255, 0, 0.0)"
         };
-console.log(this.state.regions)
+        
         return (
             <React.Fragment>
-                <div style={{ color: "white" }}>
-                  <h2>
-                    Select region on the image below and input field will appear.
-                  <br/>
-                  You can close this field with the cross button.
-                      <br/>
-                  Cross button disappears if you hover out of image.
-                  </h2>
+              <br/>
+              <br/>
+                <br/>
+                <div style={{color: "white"}}>
+                    <h4>
+                       1. Select region on the image below, an input field will appear.
+                        <br/>
+                        2. click on generate data
+                        <br/>
+                        3. Click on download data, a text file with the name and co-ordiantes will get downloaded
+                        <br/>
+                       4. You can close this field with the cross button.
+                        <br/>
+                       5. Cross button disappears if you hover out of image.
+                       <br/>
+                       
+                        <br/>
+                    </h4>
                 </div>
                 <br/>
                 <br/>
@@ -159,7 +158,7 @@ console.log(this.state.regions)
                             onChange={this.onChange}
                             regionRenderer={this.regionRenderer}
 
-                            >
+                        >
                             <img
                                 src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
                                 width="100%"
@@ -168,14 +167,22 @@ console.log(this.state.regions)
                         </RegionSelect>
                     </div>
                 </div>
+                <CsvDownload
+                    disabled={this.state.data.length > 0 === true ? false : true}
+                    data={this.state.data}
+                    filename={"dataInCsv"}
+                    className="btn btn-outline-primary btnstyle2">
+                    Download Data
+                </CsvDownload>
+
                 <button
-                type="submit"
-                className="btn btn-outline-primary btnstyle2"
-               onClick={()=>this.createTextFile()}
-              >
-                Submit
-              </button><br />
-        <br />  <br />
+                    type="submit"
+                    className="btn btn-outline-primary btnstyle2"
+                    onClick={() => this.createTextFile()}
+                >Generate Data
+                </button>
+                <br/>
+                <br/> <br/>
             </React.Fragment>
         );
     }
