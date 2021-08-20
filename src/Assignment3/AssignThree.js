27d57302bb119badaea786eb07b88e42f2933374
sunita <sunita@metromaxsolutions.com>
2021-08-20 19:41:58 +0530

    changes made

--- src/Assignment3/AssignThree.js
+++ src/Assignment3/AssignThree.js
@@ -1,281 +1,182 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import RegionSelect from "react-region-select";
-import { ValidatorForm} from "react-material-ui-form-validator";
-import { DropzoneArea } from "material-ui-dropzone";
-import { saveImageAction } from "../action/assignmentOneAction";
+import {ValidatorForm} from "react-material-ui-form-validator";
+
 import {connect} from "react-redux";
-import { withRouter } from 'react-router';
+import {withRouter} from 'react-router';
 import ReactRegionSelect from 'react-region-select';
+import {Button} from "@material-ui/core";
+import {
+  JsonToCsv,
+  useJsonToCsv
+} from 'react-json-csv';
+import CsvDownload from 'react-json-to-csv'
 
+function CsvConverter (data){
+  const filename = 'Image-Data',
+  fields = {
+    "Name": "Name",
+    "TopLeft": "Top Left",
+    "BottomRight": "Bottom Right"
+  };
+  useJsonToCsv({data,fields,filename});
+  console.log(data,filename,fields)
 
-
- export default class AssignThree extends Component {
+}
+export default class AssignThree extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          regions: [
-            {
-              x: 9.565217391304348,
-              y: 34.195402298850574,
-              width: 81.30434782608695,
-              height: 6.3218390804597675,
-              new: false,
-              data: { index: 0 },
-              isChanging: false
-            }
-          ],
-          clickableRegions: [
-            {
-              x: 9.565217391304348,
-              y: 34.195402298850574,
-              width: 81.30434782608695,
-              height: 6.3218390804597675,
-              new: false,
-              data: { index: 0 },
-              isChanging: false
-            }
-          ]
+            regions: [],
+           
+           hideCancelButton: true,
+         
         };
-      }
-      onChange = regions => {
-        console.log("​App -> onChange -> regions", regions);
-    
-        this.setState({
-          regions: regions
-        });
-      };
-      changeRegionData(index, event) {
-        console.log("​App -> changeRegionData -> index", index);
-        const region = this.state.regions[index];
-        console.log(
-          "​App -> changeRegionData -> this.state.regions",
-          this.state.regions
-        );
-        let color;
-        switch (event.target.value) {
-          case "1":
-            color = "rgba(0, 255, 0, 0.5)";
-            break;
-          case "2":
-            color = "rgba(0, 0, 255, 0.5)";
-            break;
-          case "3":
-            color = "rgba(255, 0, 0, 0.5)";
-            break;
-          default:
-            color = "rgba(0, 0, 0, 0.5)";
-        }
-    
-        region.data.regionStyle = {
-          background: color
-        };
-        this.onChange([
-          ...this.state.regions.slice(0, index),
-          Object.assign({}, region, {
-            data: Object.assign({}, region.data, { dataType: event.target.value })
-          }),
-          ...this.state.regions.slice(index + 1)
-        ]);
-      }
-      actionDeleteRegion = regionIdx => {
-        console.log("​regionIdx", regionIdx);
-        const filteredRegion = this.state.regions.filter(
-          reg => reg.data.index !== regionIdx
-        );
-        this.setState({ regions: filteredRegion });
-      };
-      regionRenderer = regionProps => {
-        if (!regionProps.isChanging) {
-          console.log("​regionRenderer -> regionProps", regionProps);
-          return (
-            <div>
-              <div style={{ position: "absolute", right: 0, top: "-25px" }}>
-                <button
-                  onClick={() => this.actionDeleteRegion(regionProps.data.index)}
-                >
-                  Cancel
-                </button>
-              </div>
-              <div style={{ position: "absolute", right: 0, bottom: "-30px" }}>
-                <select
-                  onChange={event =>
-                    this.changeRegionData(regionProps.index, event)
-                  }
-                  value={regionProps.data.dataType}
-                >
-                  <option value="1">Green</option>
-                  <option value="2">Blue</option>
-                  <option value="3">Red</option>
-                </select>
-                <button onClick={this.actionAddRegion}>Save</button>
-              </div>
-            </div>
-          );
-        }
-      };
-      actionAddRegion = () => {
-        alert("actionAddRegion");
-      };
-    
-      renderClickableRegions() {
-        const { regions } = this.state;
-        return regions.map(c => (
-          <a href="https://www.danaruma.com">
-            <div
-              style={{
-                position: "absolute",
-                width: `${c.width}%`,
-                height: `${c.height}%`,
-                top: `${c.y}%`,
-                left: `${c.x}%`,
-                background: "rgba(0, 255, 255, 0.5)"
-              }}
-            />
-          </a>
-        ));
-      }
-    
-      renderLiveDemo = () => (
-        <React.Fragment>
-          <img
-            src="https://f1-styx.imgix.net/danaruma/homepage/benefit.jpg?w=1000&h=1500&fit=crop&crop=top"
-            width="100%"
-          />
-          {this.renderClickableRegions()}
-        </React.Fragment>
-      );
-    
-      render() {
-        const regionStyle = {
-          background: "rgba(0, 255, 0, 0.5)"
-        };
-    
-        return (
-          <React.Fragment>
-            <div style={{ backgroundColor: "red" }}>
-              <h1>
-                *select region on the left side and it will become link region on
-                the right side
-              </h1>
-            </div>
-            <br />
-            <br />
-            <div style={{ display: "flex" }}>
-              <div
-                style={{
-                  flexGrow: 1,
-                  flexShrink: 1,
-                  width: "50%",
-                  left: "0",
-                  // padding: 15,
-                  position: "absolute",
-                  display: "inline-block"
-                }}
-              >
-                <RegionSelect
-                  regions={this.state.regions}
-                  regionStyle={regionStyle}
-                  constraint
-                  onChange={this.onChange}
-                  regionRenderer={this.regionRenderer}
-                  // style={{ width: "50%" }}
-                >
-                  <img
-                    src="https://f1-styx.imgix.net/danaruma/homepage/benefit.jpg?w=1000&h=1500&fit=crop&crop=top"
-                    width="100%"
-                  />
-                  {/* {this.renderClickableRegions()} */}
-                </RegionSelect>
-              </div>
-              <div
-                style={{
-                  flexGrow: 1,
-                  flexShrink: 1,
-                  width: "50%",
-                  left: "50%",
-                  // padding: 15,
-                  position: "absolute",
-                  display: "inline-block"
-                }}
-              >
-                {this.renderLiveDemo()}
-                {/* Select something with your mouse on the left side
-                <br />
-                {JSON.stringify(this.state.regions, null, 2)} */}
-              </div>
-            </div>
-          </React.Fragment>
-        );
-      }
     }
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       files: [],
-     
-//     };
-//   }
-
-//   handleChangeFile = (files) => {
-//     this.setState({
-//       files: files,
-//     });
-//   };
- 
 
 
+    showCancelButton = () => {
+        this.setState({
+            hideCancelButton: false
+        })
+    }
+ handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+    hideCancelButton = () => {
+        this.setState({
+            hideCancelButton: true
+        })
+    }
+
+
+    onChange = regions => {
+      
+        this.setState({
+            regions: regions,
+            
+        });
+    };
+    enterInput=(e,index)=>{
+      e.preventDefault();
+     const regions=this.state.regions;
+
+     for(let i of regions ){
+         if(i.data.index===index){
+           i.regionName=e.target.value;
+         }
+     }
+    this.setState({regions});
+    
+    }
+    createTextFile=()=>{
+    let data=[]
+    for(let i of this.state.regions){
+      let a={};
+      if(i.regionName){
+        a.Name=i.regionName;
+        a.TopLeft=Math.round(i.x)+","+Math.round(i.y);
+        a.BottomRight=Math.round(i.x+i.width)+","+Math.round(i.y+i.height);
+        data.push(a);
+      }
+      CsvConverter(data);
+    }
+    
+    }
   
-//   handleSubmit=(e)=>{
-//       e.preventDefault();
-//       let request={
-//           image:this.state.files,
-          
-//       };
-// this.props.saveImageAction(request);
-// this.props.history.push("/assignment")
-//   }
 
-//   render() {
-//       console.log(this.props.reduxState);
-//     return (
-//       <>
-//         <br />
-//         <br />
-//         <h1 style={{color:"white"}}>Add your images here...</h1>
-//         <br />
-//         <div className="row clearfix">
-//           <div className="col-lg-3 col-md-12"></div>
-//           <div className="col-lg-5 col-md-12">
-//             <ValidatorForm
-//               className="form1"
-//               noValidate
-//               onSubmit={this.handleSubmit}
-//             >
-//              <RegionSelect
-//     maxRegions={5}
-//     regions={this.state.regions}
-//     onChange={this.onChange}
-//     regionRenderer={this.regionRenderer}>
-//         <img src='/static/example-doc.jpg' width='700px'/>
-// </RegionSelect>
-//             </ValidatorForm>
-//           </div>
-//           <div className="col-lg-3 col-md-12"></div>
-//         </div>
-//         <br />
-//         <br />
-//         <br />
-//       </>
-//     );
-//   }
-// }
-// const mapStateToProps = (state) => ({
-//    reduxState:state
+    actionDeleteRegion = regionIdx => {
+        console.log("regionIdx", regionIdx);
+        const filteredRegion = this.state.regions.filter(
+            reg => reg.data.index !== regionIdx
+        );
+        this.setState({regions: filteredRegion});
+    };
 
-// })
-// const mapDispatchToProps = dispatch => ({
-  
-//     saveImageAction: (e) => dispatch(saveImageAction(e)),
-// })
-// export default connect(mapStateToProps, mapDispatchToProps)(withRouter(AssignThree))
+
+    regionRenderer = regionProps => {
+        if (!regionProps.isChanging) {
+            return (
+                <div>
+                    <div style={{position: "absolute", right: 0, top: "-25px"}} hidden={this.state.hideCancelButton}>
+                        <Button  variant="contained" color="secondary"
+                            onClick={() => this.actionDeleteRegion(regionProps.data.index)}
+                        >
+                            X
+                        </Button>
+                    </div>
+                    <input autoFocus onChange={(e)=>this.enterInput(e,regionProps.data.index)} placeholder={"Enter label"} className={"hiddeninput"}/>
+                </div>
+            );
+        }
+    };
+
+    actionAddRegion = () => {
+        alert("actionAddRegion");
+    };
+
+   
+
+    render() {
+        const regionStyle = {
+            background: "rgba(0, 255, 0, 0.0)"
+        };
+console.log(this.state.regions)
+        return (
+            <React.Fragment>
+                <div style={{ color: "white" }}>
+                  <h2>
+                    Select region on the image below and input field will appear.
+                  <br/>
+                  You can close this field with the cross button.
+                      <br/>
+                  Cross button disappears if you hover out of image.
+                  </h2>
+                </div>
+                <br/>
+                <br/>
+                <div style={{display: "flex"}}>
+                    <div onMouseEnter={this.showCancelButton} onMouseLeave={this.hideCancelButton}
+                         style={{
+                             flexGrow: 1,
+                             flexShrink: 1,
+                             width: "50%",
+                             left: "0",
+                             // padding: 15,
+                             position: "absolute",
+                             display: "inline-block"
+                         }}
+                    >
+                        <RegionSelect
+                            regions={this.state.regions}
+                            regionStyle={regionStyle}
+                            constraint
+                            onChange={this.onChange}
+                            regionRenderer={this.regionRenderer}
+
+                            >
+                            <img
+                                src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                                width="100%"
+                            />
+                            {/* {this.renderClickableRegions()} */}
+                        </RegionSelect>
+                    </div>
+                </div>
+                <button
+                type="submit"
+                className="btn btn-outline-primary btnstyle2"
+               onClick={()=>this.createTextFile()}
+              >
+                Submit
+              </button><br />
+        <br />  <br />
+            </React.Fragment>
+        );
+    }
+}
